4d7cfe94dacc7a17fcad5269723909cfdf5a3d98
Abdullah <visualsbyabd257@gmail.com>
2025-12-13 03:21:41 +0200

    Video Router

---
+++ Web/v1/app/api/video.ts
@@ -0,0 +1,16 @@
+// Videos Route
+// This file defines the routes for handling video-related operations in an Express application.
+
+import { NextRequest, NextResponse } from "next/server";
+
+// Get a Video by ID or Get all Videos
+export const GET = async (req: NextRequest, _: NextResponse) => {};
+
+// Create a new Video
+export const POST = async (req: NextRequest, _: NextResponse) => {};
+
+// Update a Video by ID
+export const PUT = async (req: NextRequest, _: NextResponse) => {};
+
+// Delete a Video by ID
+export const DELETE = async (req: NextRequest, _: NextResponse) => {};
