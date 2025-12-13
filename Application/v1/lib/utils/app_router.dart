f1e6bacab2b7bc567e3ccfee5f511f20e9ad9cd7
Abdullah <visualsbyabd257@gmail.com>
2025-12-13 03:14:44 +0200

    App Router

---
+++ Application/v1/lib/utils/app_router.dart
@@ -0,0 +1,28 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:v1/screens/CategoryPage.dart';
+import 'package:v1/screens/HomePage.dart';
+
+@AutoRouterConfig()
+class AppRouter extends RootStackRouter {
+  @override
+  RouteType get defaultRouteType => RouteType.material();
+
+  @override
+  List<AutoRoute> get routes => [
+    NamedRouteDef(
+      name: "HomePagePath",
+      path: "/",
+      initial: true,
+      builder: (context, data) {
+        return HomePage();
+      },
+    ),
+    NamedRouteDef(
+      name: "CategoryPath",
+      path: "/category/:categoryId",
+      builder: (context, data) {
+        return CategoryPage(categoryId: data.params.getString("categoryId"));
+      },
+    ),
+  ];
+}
